f370a6565e3e6b017d6f984f1354f503a895fba5
NaejNunes <jeannobreganunes@hotmail.com>
2019-05-22 22:25:16 -0300

    Iniciando a movimentação

---
+++ ProjetoPacMan/Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMove : MonoBehaviour
+{
+
+    public float speed = 0.4f;
+
+    Vector2 dest = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dest = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("CIMA");
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("Baixo");
+
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("Esquerda");
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Direita");
+
+        }
+    }
+}
